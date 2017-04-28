b6c5bb13523f7181a1f7cb8ccd89e6285e43a930
David Weinstein <dwein9@gmail.com>
2017-04-28 19:35:21 -0400

    added longer slide effect

---
+++ index.js
@@ -0,0 +1,9 @@
+$(document).ready(setInterval(function (){
+    $(".text").delay(1000).animate({
+        left: '56px',
+        top: '97px'
+    });
+
+    $(".text ").css("left", "-180px");
+
+}, 2000));
